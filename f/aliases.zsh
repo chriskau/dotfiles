6de325d44619b70b986aa5a419560e43128522e6
Chris Kau <chris.b.kau@gmail.com>
2011-12-27 17:15:44 -0800

    added 'f' (https://github.com/clvv/f)

---
+++ f/aliases.zsh
@@ -0,0 +1,4 @@
+alias v='f -e vim' # quick opening files with vim
+alias j='d -e cd' # quick cd into directories, mimicking autojump and z
+alias o='a -e xdg-open' # quick opening files with xdg-open
+
