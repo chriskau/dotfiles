faf51723e4230592ff919b76458cf580acd16d74
Chris Kau <chris.b.kau@gmail.com>
2012-07-29 12:50:05 -0700

    adds vim editor config

---
+++ zsh/editor.zsh
@@ -0,0 +1,8 @@
+export EDITOR="vim"
+bindkey=v
+
+# vi style incremental search
+bindkey '^R' history-incremental-search-backward
+bindkey '^S' history-incremental-search-forward
+bindkey '^P' history-search-backward
+bindkey '^N' history-search-forward
